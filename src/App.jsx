4e9264d1b44e21e3600b9d48fce86629ce709ac9
jcaicedo <jose.caicedo@outlook.com>
2023-01-05 10:02:41 -0500

    implement input search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
+import { GifGrid } from "./components/GifGrid"
+
 export const App = () => {
+
+  const [categories, setCategories] = useState(['Dragon ball'])
+
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory.toLowerCase())) return;
+
+    setCategories([...categories, newCategory])
+  }
   return (
     <>
-      <div>GifExpert</div>
+      {/* Titulo */}
+      <h1>GifExpert</h1>
+
+      {/* Input */}
+      <AddCategory onNewCategory={onAddCategory} />
+
+      {/* Listado de Gif */}
+      <GifGrid />
+
+      {categories.map(category =>
+        <GifGrid key={category}
+          category={category} />
+      )}
     </>
   )
 }
